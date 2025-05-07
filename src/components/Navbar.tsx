f841bb701bec54ca14c6edf27d55cc38c4188bfc
Uriel Piñeyro <uriel.pineyro@gmail.com>
2025-05-07 17:35:00 -0300

    Se trabaja en el navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-      <Link to="/favourites">
-        <button>Favoritos</button>
-      </Link>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-    </>
+    <div className="flex items-center justify-between py-5 font-medium">
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1 ">
+          <p>Home</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/favourites" className="flex flex-col items-center gap-1 ">
+          <p>Favourites</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+
+        <NavLink to="/login" className="flex flex-col items-center gap-1 ">
+          <p>Login</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+    </div>
   );
 }
 
